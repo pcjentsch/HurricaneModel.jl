00930d0b2401bc622094ffdf36125d34b83cd70f
pjentsch <pjentsch@uwaterloo.ca>
2021-07-19 12:15:30 -0400

    switch to OWID source

--- src/data.jl
+++ src/data.jl
@@ -8,7 +8,7 @@ function fetch_data_by_country()
     covid_cases_data = CSV.File(covid_cases_path) |> DataFrame
     cases_dates_as_strings = names(covid_cases_data)[5:end]
     # rename!(covid_cases_data,Symbol.(names(covid_cases_data)))
-    display(covid_cases_data)
+    # display(covid_cases_data)
     # covid_recovered_path = joinpath(PACKAGE_FOLDER,"data","csse_covid_19_data","csse_covid_19_time_series","time_series_covid19_recovered_global.csv")
     # covid_recovered_data = CSV.File(covid_recovered_path) |> DataFrame
     
@@ -47,9 +47,55 @@ function fetch_data_by_country()
     
     return region_cases
 end
+function is_contiguous(l)
+    for i in 1:length(l)-1
+        d = l[i]
+        if l[i+1] != d + Day(1)
+            return false
+        end
+    end
+    return true
+end
+
+function fetch_data_by_country_owid()
+    
+    covid_cases_path = joinpath(PACKAGE_FOLDER,"data","owid-covid-data.csv")
+    covid_cases_data = CSV.File(covid_cases_path) |> DataFrame
+
+    locations = unique(covid_cases_data[:,:location])
+    location_data_dict = Dict{String,LocationData}()
+    for location in locations
+        location_subset_w_missing = filter(:location =>  ==(location),covid_cases_data)
+        sort!(location_subset_w_missing,:date)
+        location_subset = dropmissing(location_subset_w_missing, [:new_cases_smoothed,:new_vaccinations_smoothed,:total_cases])
+        new_cases_smoothed = location_subset[:,:new_cases_smoothed]       
+        total_cases= location_subset[:,:total_cases]
+        new_vaccinations_smoothed = location_subset[:, :new_vaccinations_smoothed]
+        dates = location_subset[:,:date]
+        if length(total_cases) >0
+            population = location_subset[:,:population][1]
+            if !is_contiguous(dates)
+                display((location,dates))
+            else
+                display("contiguous!")
+            end 
+            location_data = LocationData(
+                total_cases,
+                new_cases_smoothed,
+                new_vaccinations_smoothed,
+                dates,
+                population
+            )
+            location_data_dict[location] = location_data
+        end
+    end
+    return location_data_dict
+end
 
 struct LocationData
-    cases::Vector{Float64} #total cases by day
+    total_cases::Vector{Float64} #total cases by day
+    new_cases::Vector{Float64} #total cases by day
+    new_vaccinations::Vector{Float64} #total cases by day
     dates::Vector{Date} #date corresponding to total cases
     population::Float64
 end
